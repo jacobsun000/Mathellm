914128d3d470be276635310a4574e5fb999e8173
Jacob Sun <jacobsun000@gmail.com>
2024-10-29 00:38:19 -0500

    doc: Add doc to main
    doc

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import src.differential as diff
+import asyncio
 
 
 async def main():
-    inputs = diff.DiffConstant.generate_random_inputs()
-    problem = diff.DiffConstant(**inputs)
+    # Select a problem class
+    problem_class = diff.DiffConstant
+    # Generate raw problem with CAS
+    inputs = problem_class.generate_random_inputs()
+    problem = problem_class(**inputs)
+    # Wrap the problem content with LLM
     await problem.wrap_with_llm()
+    # Display the problem
     print(problem)
 
 
 if __name__ == "__main__":
-    import asyncio
-
     asyncio.run(main())
